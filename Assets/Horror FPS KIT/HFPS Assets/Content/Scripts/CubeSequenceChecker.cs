905f5ae4d798c6015ee64174614a528bb3fdf6ca
Jun Huang <junh@gatech.edu>
2023-10-26 13:28:47 +0200

    added hint text and changed the cube color settings

--- Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/CubeSequenceChecker.cs
+++ Assets/Horror FPS KIT/HFPS Assets/Content/Scripts/CubeSequenceChecker.cs
@@ -1,37 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CubeSequenceChecker : MonoBehaviour
 {
     public GameObject HEROPLAYER;
     public GameObject[] cubes;
     public float detectionDistance = 2.0f;
+    public TextMeshProUGUI hintText; // Assign this in the inspector
 
     private int[] targetSequence = { 6, 4, 5, 7 };
     private List<int> visitedSequence = new List<int>();
     private Renderer[] cubeRenderers;
+    private float startTime;
 
     private void Start()
     {
         cubeRenderers = new Renderer[cubes.Length];
+        startTime = Time.time;
+
         for (int i = 0; i < cubes.Length; i++)
         {
-            // Initialize renderers
             cubeRenderers[i] = cubes[i].GetComponent<Renderer>();
-
-            // Set up the text on top of cubes
-            GameObject textObj = new GameObject($"CubeText{i}");
-            textObj.transform.SetParent(cubes[i].transform);
-            textObj.transform.localPosition = new Vector3(0, 0.7f, 0);
-            TextMesh textMesh = textObj.AddComponent<TextMesh>();
-            textMesh.text = i.ToString();
-            textMesh.alignment = TextAlignment.Center;
-            textMesh.anchor = TextAnchor.MiddleCenter;
-            textMesh.fontSize = 500;
-            textMesh.characterSize = 0.02f;
-            textMesh.color = Color.black;
+            CreateNumberOnCube(i);
         }
+
+        ShowHint("Welcome player, now you see 10 boxes in the room, please visit each one to figure out what boxes are important for solving the puzzle", 10f);
+
+        // Center the hintText in the screen
+        RectTransform rt = hintText.GetComponent<RectTransform>();
+        rt.anchorMin = new Vector2(0.5f, 0.5f);
+        rt.anchorMax = new Vector2(0.5f, 0.5f);
+        rt.pivot = new Vector2(0.5f, 0.5f);
+        rt.localPosition = Vector3.zero;
     }
 
     private void Update()
@@ -44,23 +46,36 @@ public class CubeSequenceChecker : MonoBehaviour
                 {
                     ChangeColorTemporarily(i, Color.red, 1f);
 
-                    // Record the visit
-                    if (!visitedSequence.Contains(i)) // Avoid adding the same cube multiple times
+                    if (!visitedSequence.Contains(i))
                         visitedSequence.Add(i);
                     
-                    // Check if the last four visits match the target sequence
-                    if (visitedSequence.Count >= 4 
-                        && visitedSequence[visitedSequence.Count - 4] == targetSequence[0] 
-                        && visitedSequence[visitedSequence.Count - 3] == targetSequence[1]
-                        && visitedSequence[visitedSequence.Count - 2] == targetSequence[2]
-                        && visitedSequence[visitedSequence.Count - 1] == targetSequence[3])
+                    if (visitedSequence.Count >= 4 && CheckSequence())
                     {
                         ChangeColorOfSequenceCubes(Color.green);
-                        visitedSequence.Clear(); // Clear the sequence after matching
+                        StartCoroutine(ChangeAllCubesToGreenAfterDelay(10f));
+                        visitedSequence.Clear();
                     }
                 }
             }
         }
+
+        if (Time.time - startTime > 60f && hintText.text == "") // After 1 minute, show the second hint
+        {
+            ShowHint("Now you might have noticed that four boxes are related to the game, now please visit them again in a sequence. Think about the course code of video game design, it is CS-6 what?", 10f);
+        }
+    }
+
+    private void CreateNumberOnCube(int cubeIndex)
+    {
+        GameObject textObj = new GameObject($"CubeText{cubeIndex}");
+        textObj.transform.SetParent(cubes[cubeIndex].transform);
+        textObj.transform.localPosition = new Vector3(0, 0.7f, 0);
+        TextMeshPro textMeshPro = textObj.AddComponent<TextMeshPro>();
+        textMeshPro.text = cubeIndex.ToString();
+        textMeshPro.alignment = TextAlignmentOptions.Center;
+        textMeshPro.fontSize = 10; // Adjust as needed
+        textMeshPro.color = Color.black;
+        textMeshPro.GetComponent<RectTransform>().sizeDelta = new Vector2(2, 2); // Adjust as needed
     }
 
     private void ChangeColorTemporarily(int cubeIndex, Color color, float duration)
@@ -73,7 +88,7 @@ public class CubeSequenceChecker : MonoBehaviour
         Color originalColor = cubeRenderer.material.color;
         cubeRenderer.material.color = color;
         yield return new WaitForSeconds(duration);
-        if(cubeRenderer.material.color == color) // Only revert the color if it hasn't changed in the meantime
+        if (cubeRenderer.material.color == color)
             cubeRenderer.material.color = originalColor;
     }
 
@@ -83,5 +98,57 @@ public class CubeSequenceChecker : MonoBehaviour
         {
             cubeRenderers[index].material.color = color;
         }
+    }
+
+    private IEnumerator ChangeAllCubesToGreenAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        foreach (Renderer cubeRenderer in cubeRenderers)
+        {
+            cubeRenderer.material.color = Color.green;
+        }
+    }
+
+    private bool CheckSequence()
+    {
+        if (visitedSequence.Count >= 4)
+        {
+            int count = visitedSequence.Count;
+            return visitedSequence[count - 4] == targetSequence[0] 
+                && visitedSequence[count - 3] == targetSequence[1]
+                && visitedSequence[count - 2] == targetSequence[2]
+                && visitedSequence[count - 1] == targetSequence[3];
+        }
+        return false;
+    }
+
+    private void ShowHint(string message, float duration)
+    {
+        hintText.text = message;
+        hintText.fontSize = 24; // Adjust as needed
+
+        RectTransform rt = hintText.GetComponent<RectTransform>();
+        
+        // Set the anchors to stretch horizontally
+        rt.anchorMin = new Vector2(0, 0.5f);
+        rt.anchorMax = new Vector2(1, 0.5f);
+        
+        // Set the sizeDelta to adjust height
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, 100); // You can adjust the height as needed
+        
+        
+        // Optional: Add some padding
+        rt.offsetMin = new Vector2(rt.offsetMin.x + 10, rt.offsetMin.y); // Left padding
+        rt.offsetMax = new Vector2(rt.offsetMax.x - 10, rt.offsetMax.y); // Right padding
+
+        StartCoroutine(ClearHintAfterDelay(duration));
+    }
+
+
+
+    private IEnumerator ClearHintAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        hintText.text = "";
     }
 }
