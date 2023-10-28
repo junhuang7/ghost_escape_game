f58ecb5e72470f3d3fbe0526c60e3685834869ab
Jun Huang <junh@gatech.edu>
2023-10-29 00:59:50 +0200

    door anim and code updated

--- Assets/Scripts/DoorOpenController.cs
+++ Assets/Scripts/DoorOpenController.cs
@@ -1,13 +1,36 @@
+using System.Collections;
 using UnityEngine;
 
 public class DoorOpenController : MonoBehaviour
 {
-    public Animator doorAnimator; // Assign this in the inspector
-    private bool isOpen = false;
+    private bool isDoorOpen = false;
+    private Animation doorAnimation;
+
+    private void Start()
+    {
+        // Get the Animation component attached to this GameObject
+        doorAnimation = GetComponent<Animation>();
+        // Check if the Animation component or the Animation Clip is missing
+        if (doorAnimation == null)
+        {
+            Debug.LogError("Animation component is missing!");
+        }
+        else if (doorAnimation.GetClip("Door_Open") == null)
+        {
+            Debug.LogError("Animation clip 'Door_Open' is missing!");
+        }
+    }
 
     public void ToggleDoor()
     {
-        isOpen = !isOpen;
-        doorAnimator.SetBool("isOpen", isOpen);
+        if (!isDoorOpen)
+        {
+            // Play the "Door_Open" animation
+            if (doorAnimation != null)
+            {
+                doorAnimation.Play("Door_Open");
+            }
+            isDoorOpen = true;
+        }
     }
 }
